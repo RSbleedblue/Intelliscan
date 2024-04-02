c60e6dde0440d11e286d094511502bef351f443a
Rivansh <rivansh63@gmail.com>
2024-04-02 17:14:49 +0530

    adding extra file

--- homeScript.js
+++ homeScript.js
@@ -1,44 +1,35 @@
-function takeMe(){
-    const createResumeSection = document.getElementById("create-Resume");
-    createResumeSection.classList.add('smooth-scroll');
-    createResumeSection.scrollIntoView({behavior:'smooth'});
-    console.log("script Working");
+function takeMe() {
+  const createResumeSection = document.getElementById("create-Resume");
+  createResumeSection.classList.add("smooth-scroll");
+  createResumeSection.scrollIntoView({ behavior: "smooth" });
+  console.log("script Working");
 }
 
-const textarea = document.getElementById('summary');
-const workHistory = document.getElementById('workHistory');
+const textarea = document.getElementById("summary");
+const workHistory = document.getElementById("workHistory");
 function adjustTextareaHeight(textarea) {
-    textarea.style.height = 'auto';
-    textarea.style.height = (textarea.scrollHeight) + 'px';
+  textarea.style.height = "auto";
+  textarea.style.height = textarea.scrollHeight + "px";
 
-    const containerDiv = textarea.closest('.w-full');
-    containerDiv.style.height = (textarea.scrollHeight) + 'px';
+  const containerDiv = textarea.closest(".w-full");
+  containerDiv.style.height = textarea.scrollHeight + "px";
 }
 
-document.getElementById('summary').addEventListener('input', function() {
-    adjustTextareaHeight(this);
+document.getElementById("summary").addEventListener("input", function () {
+  adjustTextareaHeight(this);
 });
-document.getElementById('workHistory').addEventListener('input', function() {
-    adjustTextareaHeight(this);
+document.getElementById("workHistory").addEventListener("input", function () {
+  adjustTextareaHeight(this);
 });
-document.getElementById('education').addEventListener('input',function(){
-    adjustTextareaHeight(this);
+document.getElementById("education").addEventListener("input", function () {
+  adjustTextareaHeight(this);
 });
-document.getElementById("skills").addEventListener('input',function(){
-    adjustTextareaHeight(this);
-})
+document.getElementById("skills").addEventListener("input", function () {
+  adjustTextareaHeight(this);
+});
 
-
-function generatePDF(){
-    const element = document.getElementById("create-Resume");
-    var opt = {
-        margin:       1,
-        filename:     'myfile.pdf',
-        image:        { type: 'jpeg', quality: 0.98 },
-        html2canvas:  { scale: 2 },
-        jsPDF:        { unit: 'in', format: 'letter', orientation: 'portrait' }
-      };
-      console.log(element);
-      console.log(window);
-    html2pdf().from(element).set(opt).save();
-}
+$(document).ready(function () {
+  $("#submitbtn").click(function () {
+    $("#create-Resume").printThis();
+  });
+});
